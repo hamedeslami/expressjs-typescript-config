0cf4d43960843a5f7a13355fbb079a644e816f74
Hamed Eslami <hamedeslami.ir@gmail.com>
2025-03-28 22:03:34 +0330

    remove unnecessary route

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import AuthRouter from './modules/auth/auth.routes';
 import UserRouter from './modules/user/user.routes';
-import BrandRouter from './modules/store/products/brand/brand.routes';
 import RbacRouter from './modules/rbac/rbac.routes';
 
 
@@ -10,8 +9,5 @@ const mainRouter = Router();
 mainRouter.use('/auth', AuthRouter);
 mainRouter.use('/user', UserRouter);
 mainRouter.use('/rbac', RbacRouter);
-
-// store url
-mainRouter.use('/store/products/brands', BrandRouter)
 
 export default mainRouter;
